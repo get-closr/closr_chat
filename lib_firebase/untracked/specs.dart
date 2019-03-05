babab3aa876ef3fc21d91b95198fd3b58de87267
Wan Yi <wanyizhou.z@gmail.com>
2019-03-06 00:44:45 +0800

    removed untracked

--- lib_firebase/untracked/specs.dart
+++
@@ -1,5 +0,0 @@
-const googleAppID = "1:94320095180:android:76cbcbf742825ec6";
-const gcmSenderID = "94320095180";
-const apiKey = "AIzaSyABcU0VQiNY1V4BAT8PCbMLN7MGsXuWAZs";
-const projectID = "closrchat";
-const storageBucket = 'gs://closrchat.appspot.com';
